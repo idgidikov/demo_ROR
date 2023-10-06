4cbbcf2524ce68df2dc31af8feee7208ac66a01a
idgidikov <idgidikov@abv.bg>
2023-10-06 15:34:52 +0300

    ror demo

---
+++ demo/app/javascript/application.js
@@ -0,0 +1,8 @@
+// Configure your import map in config/.rb. Read more: https://github.com/rails/-rails
+import "@hotwired/turbo-rails"
+import "controllers"
+
+import "trix"
+import "@rails/actiontext"
+import LocalTime from "local-time"
+LocalTime.start()
